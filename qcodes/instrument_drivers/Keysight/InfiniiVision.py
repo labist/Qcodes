dbd6587a78ec66aea920bd154654daf5ca52c789
Endeavour_Data_Computer <jorden.senior@ist.ac.at>
2021-03-03 08:06:23 +0100

    infinivision: get signed data

--- qcodes/instrument_drivers/Keysight/InfiniiVision.py
+++ qcodes/instrument_drivers/Keysight/InfiniiVision.py
@@ -267,12 +267,13 @@ class RawTrace(ArrayParameter):
         instr._parent.data_source('CHAN{}'.format(self._channel))
         # specifiy the data format in which to read
         instr.write(':WAVeform:FORMat WORD')
+        instr.write(':WAVeform:UNSigned OFF')
         instr.write(":waveform:byteorder LSBFirst")
         # instr.write(":WAVeform:UNSigned 1")
 
         # request the actual transfer
         data = instr._parent.visa_handle.query_binary_values(
-            'WAV:DATA?', datatype='H', is_big_endian=False)
+            'WAV:DATA?', datatype='h', is_big_endian=False)
         # the Infiniium does not include an extra termination char on binary
         # messages so we set expect_termination to False
         channel_data = np.array(data)
